1d528206e642a61cfdd42f33adcfc139b8b9119d
timil <timilvaishnav@gmail.com>
2025-08-26 18:56:07 +0530

    final

--- src/components/SchoolSearch.jsx
+++ src/components/SchoolSearch.jsx
@@ -4,8 +4,7 @@ import { useState } from 'react';
 import SchoolCard from './SchoolCard';
 import Statistics from './Statistics';
 
-const API_BASE_URL = import.meta.env.API_BASE_URL;
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
 
 const SchoolSearch = ({ onAlert }) => {
   const [userLocation, setUserLocation] = useState({ latitude: '', longitude: '' });
